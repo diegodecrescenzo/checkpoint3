6b6f50aaaba5120ec63585f94c510b916e576f76
Diego Decrescenzo <diego.mesadecrescenzo@gmail.com>
2021-09-28 17:58:11 +0200

    Ultimos Upgrades

--- js/form.js
+++ js/form.js
@@ -1,19 +1,19 @@
-const pesquisaCep = () => {    
-    
-    const cep = inputcep.value;    
+const pesquisaCep = () => {
+
+    const cep = inputcep.value;
     const url = `http://viacep.com.br/ws/${cep}/json/`;
 
     return fetch(url)
-    .then( responseData =>{
-        return responseData.json();
-    })
-    .then(data =>{
-        preencherCampos(data)
-    })   
-    
+        .then(responseData => {
+            return responseData.json();
+        })
+        .then(data => {
+            preencherCampos(data)
+        })
+
 }
 
-const preencherCampos = (data) =>{
+const preencherCampos = (data) => {
 
     //console.log(data);
 
@@ -21,11 +21,11 @@ const preencherCampos = (data) =>{
     const bairro = document.querySelector('#bairro');
     const cidade = document.querySelector('#cidade');
     const estado = document.querySelector('#estado');
-    
+
     endereco.value = data.logradouro;
     bairro.value = data.bairro;
     cidade.value = data.localidade;
-    estado.value = data.uf;    
+    estado.value = data.uf;
 }
 
 const inputcep = document.querySelector('#cep');
@@ -34,13 +34,20 @@ inputcep.addEventListener('blur', pesquisaCep);
 
 
 const btnSubmit = document.querySelector('[data-submit]');
-btnSubmit.addEventListener('click', (e) =>{
+btnSubmit.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const template = `<h3 class="bg-success text-white">Seu Pedido foi realizado com sucesso</h3>`
+    const template = `<h3 class="bg-success text-white mt-4">Seu Pedido foi realizado com sucesso</h3>`
 
-    const alert = document.querySelector('#id');
-    alert.appendChild(template)
-   
-} );  
+    const alert = document.querySelector('#alert');
+    alert.innerHTML = template
+
+    document.querySelector('#nome').value = ''
+    document.querySelector('#sobrenome').value = ''
+    document.querySelector('#email').value = ''
+    document.querySelector('#pedido').value = ''
+    document.querySelector('#complemento').value = ''
+    inputcep.value = ''
+
+});
 
